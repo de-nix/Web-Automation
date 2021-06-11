89ab19ff8981827a95c25b5648b62fa861f13113
de-nix <criste.denis@yahoo.com>
2021-06-11 15:57:04 +0300

    final fix

--- GrepolisResourceCollector/src/test/java/org/example/features/search/CollectResources.java
+++ GrepolisResourceCollector/src/test/java/org/example/features/search/CollectResources.java
@@ -26,29 +26,35 @@ public class CollectResources {
 
     // this method runs continuously and calls the method collect_once from 10 to ten minutes
     @Test
-    public void collect(){
-        while (true){
-            collect_once();
-            waitForIt(60*10);
+    public void collect() {
+        while (true) {
+            try {
+                collect_once();
+                waitForIt(60 * 10);
+            } catch (Exception exception) {
+                System.err.println(exception.getMessage());
+                waitForIt(10);
+            }
         }
     }
+
     private void collect_once() {
         steps.openPage();
         // here you add your credentials- username and password
-        steps.login("Apocalypto","sebastian95");
+        steps.login("user", "password");
         waitForIt(1);
         steps.select_world();
         waitForIt(1);
         steps.hover(webdriver);
         // Here you add the indexes of the villages you want to collect resources from.
         // ATTENTION the islands are counting also to the indexing(first island is at index 1)
-        villages = List.of(2,4);
+        villages = List.of(2, 4);
         steps.collect(villages, webdriver);
         webdriver.close();
     }
+
     // this methods waits for a given amount of seconds
-    private void waitForIt(int secondsToWait)
-    {
+    private void waitForIt(int secondsToWait) {
         try {
             TimeUnit.SECONDS.sleep(secondsToWait);
         } catch (InterruptedException e) {
